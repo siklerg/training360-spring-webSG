b5dbb52393d2c5f4332bcc5579529e8a6b207e75
Siklér Gábor <sikler.gabor@innoteq.hu>
2019-04-17 15:55:26 +0200

    Erőforrások kezelése

--- src/main/java/springmvc/controller/Webconfig.java
+++ src/main/java/springmvc/controller/Webconfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
@@ -15,11 +17,16 @@ import org.thymeleaf.templateresolver.ITemplateResolver;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackageClasses = Webconfig.class)
-public class Webconfig {
+public class Webconfig implements WebMvcConfigurer{
 
 	@Autowired
 	private ApplicationContext applicationContext;
 	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+
 	@Bean
 	public ViewResolver viewResolver() {
 	ThymeleafViewResolver resolver = new ThymeleafViewResolver();
